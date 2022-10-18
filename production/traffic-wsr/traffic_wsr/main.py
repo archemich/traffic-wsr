9c7dadf117ca443f505d1789fed0cf6982d82ff5
archemich <archemich@gmail.com>
2022-10-18 11:28:53 +0300

    setup project

--- production/traffic-wsr/traffic_wsr/main.py
+++ production/traffic-wsr/traffic_wsr/main.py
@@ -1,7 +1,25 @@
+import argparse
 
+from .version import __version__
+
+def parse_args() ->argparse.Namespace:
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--version',
+                        action='version',
+                        version=__version__)
+    args = parser.parse_args()
+    return args
 
 def main():
+    """
+    Script entry point
+    :return:
+    """
+    args = parse_args()
+
     print('hello world')
+
 
 if __name__ == '__main__':
     main()
---
+++ production/traffic-wsr/traffic_wsr/main_window.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QImage
+
+class MainWindow(QWidget):
+
+    def __init__(self):
+        pass
